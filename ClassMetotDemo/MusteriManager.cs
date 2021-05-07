fc5532dc3703b0aa27258ea5a92dbd97d6b86328
batuhan6 <32600613+batuhan6@users.noreply.github.com>
2021-05-07 19:30:06 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen musteri " + musteri.MusteriAdi +" " +musteri.MusteriSoyadi);
+            
+            //musteriler.Add(musteri);
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Silinen musteri " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+        }
+        public void MusteriListeleme(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+            }
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+
+            musteri1.MusteriAdi = "Murat";
+            musteri2.MusteriAdi = "Buket";
+            musteri3.MusteriAdi = "Dilek";
+
+            musteri1.MusteriSoyadi = "Baykal";
+            musteri2.MusteriSoyadi = "Baykal";
+            musteri3.MusteriSoyadi = "Baykal";
+
+            musteri1.Id = 1;
+            musteri2.Id = 2;
+            musteri3.Id = 3;
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkleme(musteri1);
+            musteriManager.MusteriListeleme(musteriler);
+            musteriManager.MusteriSilme(musteri2);
+        }
+    }
+}
